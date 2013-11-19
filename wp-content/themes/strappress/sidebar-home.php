707eca68db064667664ae45b12ef8da8ade9081e
antoninab <antoninab@free.fr>
2013-11-19 16:01:31 +0100

    changes home

--- wp-content/themes/strappress/sidebar-home.php
+++ wp-content/themes/strappress/sidebar-home.php
@@ -16,43 +16,15 @@
     <div id="widgets" class="home-widgets">
         <div class="row-fluid">
         <div class="span4">
-        
-        <?php responsive_widgets(); // above widgets hook ?>
-            
-            <?php if (!dynamic_sidebar('home-widget-1')) : ?>
-            
-                <div class="widget-title-home"><h3><?php _e('Home Widget 1', 'responsive'); ?></h3></div>
-                <div class="textwidget"><?php _e('This is your first home widget box. To edit please go to Appearance > Widgets and choose 6th widget from the top in area six called Home Widget 1. Title is also managable from widgets as well.','responsive'); ?></div>
-            
-			<?php endif; //end of home-widget-1 ?>
-
-        <?php responsive_widgets_end(); // responsive after widgets hook ?>
+            <img src="wp-content/themes/strappress/images/cash.png"/>            
         </div><!-- end of .span4 -->
 
         <div class="span4">
-        <?php responsive_widgets(); // responsive above widgets hook ?>
-            
-			<?php if (!dynamic_sidebar('home-widget-2')) : ?>
-            
-                <div class="widget-title-home"><h3><?php _e('Home Widget 2', 'responsive'); ?></h3></div>
-                <div class="textwidget"><?php _e('This is your second home widget box. To edit please go to Appearance > Widgets and choose 7th widget from the top in area seven called Home Widget 2. Title is also managable from widgets as well.','responsive'); ?></div>
-            
-			<?php endif; //end of home-widget-2 ?>
-            
-            <?php responsive_widgets_end(); // after widgets hook ?>
+            <img src="wp-content/themes/strappress/images/profes.png"/>
         </div><!-- end of .span4 -->
 
         <div class="span4">
-        <?php responsive_widgets(); // above widgets hook ?>
-            
-            <?php if (!dynamic_sidebar('home-widget-3')) : ?>
-            
-                <div class="widget-title-home"><h3><?php _e('Home Widget 3', 'responsive'); ?></h3></div>
-                <div class="textwidget"><?php _e('This is your third home widget box. To edit please go to Appearance > Widgets and choose 8th widget from the top in area eight called Home Widget 3. Title is also managable from widgets as well.','responsive'); ?></div>
-        
-		    <?php endif; //end of home-widget-3 ?>
-            
-        <?php responsive_widgets_end(); // after widgets hook ?>
+            <img src="wp-content/themes/strappress/images/tele.png"/>
         </div><!-- end of .span4 -->
         </div>
     </div><!-- end of #widgets -->
